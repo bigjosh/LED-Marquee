7eb45962dc445aeefe9bb478cbf136f86a0b9f12
Jeff Schaefer <blkhwks19@users.noreply.github.com>
2018-03-25 19:11:05 -0500

    Updated reading from server to use readString() instead of readStringUntil(). Added reading from UNO and responding.

--- Phase1_ESP.ino
+++ Phase1_ESP.ino
@@ -1,9 +1,9 @@
 #include <ESP8266WiFi.h>
 
-const char* ssid     = "********";
+const char* ssid = "********";
 const char* password = "********";
-
 const char* host = "********";
+const int httpPort = 80;
 
 String str = "";
 
@@ -12,50 +12,43 @@ void setup() {
   delay(10);
 
   // We start by connecting to a WiFi network
-
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
+  Serial.print("Connecting to wifi network: ");
   Serial.println(ssid);
   
   /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
      would try to act as both a client and an access-point and could cause
-     network-issues with your other WiFi-devices on your WiFi-network. */
+     network-issues with other WiFi-devices on the WiFi-network. */
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  
+
+  // print a '.' and loop until it connects
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
 
-  Serial.println("");
   Serial.println("WiFi connected");  
-  Serial.println("IP address: ");
+  Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 }
 
-int value = 0;
-
 void loop() {
-  delay(5000);
-  ++value;
+  delay(5000);  // wait a little so we're not spamming the host with requests
   str = "";
 
-  Serial.print("connecting to ");
+  Serial.print("Connecting to host: ");
   Serial.println(host);
   
   // Use WiFiClient class to create TCP connections
   WiFiClient client;
-  const int httpPort = 80;
   if (!client.connect(host, httpPort)) {
+    // if it cant connect, kick out of loop() and try again
     Serial.println("connection failed");
     return;
   }
   
   // We now create a URI for the request
   String url = "/marquee/data.txt";
-  
   Serial.print("Requesting URL: ");
   Serial.println(url);
   
@@ -63,6 +56,8 @@ void loop() {
   client.print(String("GET ") + url + " HTTP/1.1\r\n" +
                "Host: " + host + "\r\n" + 
                "Connection: close\r\n\r\n");
+
+  // if nothing is available for a few seconds, then timeout the connection, try again
   unsigned long timeout = millis();
   while (client.available() == 0) {
     if (millis() - timeout > 5000) {
@@ -72,21 +67,29 @@ void loop() {
     }
   }
   
-  // Read all the lines of the reply from server and print them to Serial
+  // Read the reply from server, store message data for later
   while(client.available()){
-    String line = client.readStringUntil('Z');
-    if (line.substring(0,4) == "HTTP") {
-      Serial.print("Skipped headers");
-    }
-    else {
-      Serial.print(line);
-      str += line;
-    }
+    str = client.readString();
+    Serial.println("BEGIN");
+    Serial.println(str);
+    Serial.println("END");
+    
+//    String line = client.readStringUntil('Z');
+//    if (line.substring(0,4) == "HTTP") {
+//      Serial.print("Skipped headers");
+//    }
+//    else {
+//      Serial.print(line);
+//      str += line;
+//    }
   }
   
-  Serial.println();
-  Serial.println("closing connection");
+  // Read from the UNO. If we get a '~' then send the custom message as a response
+  while(Serial.available()) {
+    if (Serial.readString() == "~") {
+      Serial.write(str);
+    }
+  }
 
-
-  // how to listen for the ? coming from UNO and send the message back....?
+  
 }
